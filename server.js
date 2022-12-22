1cbfee0db073336d96bfed00409f4117783a52b8
Carl Kevin Galvan <99018607+CKevinGalvan@users.noreply.github.com>
2022-12-22 09:23:19 -0800

    Update server.js

--- server.js
+++ server.js
@@ -11,3 +11,16 @@ app.listen(3000, function()
 app.get("/", function(request, response){
     response.send("<h1>Hello World!</h1>");
 });
+
+app.get("/contact", function(req, res)
+{
+res.send("Contact me ate: galvancarlkevin@gmail.com");
+});
+
+app.get("/hobbies", function(req, res){
+    res.send("<ul><li>playing instruments</li><li>reading</li><li>programming</li></ul>");
+});
+
+app.get("/about", function(req, res){
+    res.send("Mr. CK is the owner of this server!");
+});
